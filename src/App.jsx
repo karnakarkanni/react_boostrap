4be360c5e19ea0a856e05e73b239b4d0e6c0201a
karnakarmarati <karnakarmaratikanni@gmail.com>
2024-11-07 21:08:05 +0530

    cards

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Cards from './card'
+import Res_nav from './restaurent.jsx/nav'
+
+
+
+function App() {
+  return (
+    <>
+    <h1>hello</h1>
+    <Cards/>
+    {/* <Res_nav/> */}
+    </>
+  )
+}
+
+export default App
